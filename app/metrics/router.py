362dbb2bc39617bf0392995096c1dae34cfbb04e
Konstantin Grudnitskiy <k.grudnitskiy@yandex.ru>
2023-08-02 10:40:29 +0300

    Add comments to endpoints

--- app/metrics/router.py
+++ app/metrics/router.py
@@ -15,6 +15,15 @@ router = APIRouter(
 
 @router.post("", status_code=201)
 async def add_metrics(metrics: SMetrics) -> SMetrics:
+    """
+    This endpoint is used to add metrics to database
+    in following format:
+        {
+            "serviceName": "Name of the service",
+            "path": "Requested path, eg. /users/me",
+            "responseTimeMs": "Response time in ms"
+        }
+    """
     if result := await MetricsService.add(**metrics.model_dump()):
         return result
     raise AddMetricsException
@@ -22,6 +31,22 @@ async def add_metrics(metrics: SMetrics) -> SMetrics:
 
 @router.get("/{service_name}")
 async def get_metrics(service_name: str) -> List[SMetricsInfo]:
+    """
+    This endpoint fetch some statistic for requested service, grouped by path:
+        [
+            {
+                "path": "/users/me",
+                "average": "Average response time in ms",
+                "min": "Minimal response time in ms",
+                "max": "Max response time in ms",
+                "p99": "99 percentile response time"
+            },
+            {
+                "path": "/posts",
+                ...
+            }
+        ]
+    """
     try:
         return await MetricsService.get(service_name)
     except ConnectionError:
